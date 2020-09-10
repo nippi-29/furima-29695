03187dc3bdaf9f708463a0126175dd643c3a3ade
nippi-29 <pipimelody.1726@gmail.com>
2020-09-10 14:36:05 +0900

    バリデーションとエラーメッセージの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,18 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
+         
+         with_options presence: true do
+           validates :name
+           validates :email
+           validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i, message: "は6文字以上の英数字で入力してください"}
+           validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角文字で入力してください"}
+           validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角文字で入力してください"}
+           validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください"}
+           validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください"}
+           validates :birthday
+         end
+      
+
 end
