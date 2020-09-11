a114447fe7aa7bcd71afc9e0b26a762fe6e55525
nippi-29 <pipimelody.1726@gmail.com>
2020-09-11 22:22:59 +0900

    emailに＠マークがない場合登録できないようにバリデーションに追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :name
-    validates :email, uniqueness: { case_sensitive: false }
+    validates :email, uniqueness: { case_sensitive: false }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: 'は＠マークが必要です'}
     validates :password, confirmation: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i, message: 'は6文字以上の半角英数字の混合で入力してください' }
     validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角文字で入力してください'}
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角文字で入力してください'}
