39e84ad14475af5c6adda5901151d21279802010
nippi-29 <pipimelody.1726@gmail.com>
2020-09-14 03:11:50 +0900

    コントローラーの設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,25 @@ class ProductsController < ApplicationController
 
   def index
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else render :new
+    end
+  end
+
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(:image, :name, :description, :category_id, :item_status_id, :postage_id, :region_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
+
 end
