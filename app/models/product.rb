33c6bc861b885e2396e3182560ae158e6e7ea825
nippi-29 <pipimelody.1726@gmail.com>
2020-09-13 11:09:23 +0900

    アソシエーションとバリデーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,26 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :item_status
+  belongs_to_active_hash :postage
+  belongs_to_active_hash :region
+  belongs_to_active_hash :shipping_date
+  belongs_to :user
+  has_one_attached :image
+
+
+  with_option presence: true do
+    validates :name,                     length: { maximum: 40 }
+    validates :description,              length: { maximum: 1000 }
+    validates :category_id,              numericality: { other_than: 1 }
+    validates :item_status_id,           numericality: { other_than: 1 }
+    validates :postage_id,               numericality: { other_than: 1 }
+    validates :region_id,                numericality: { other_than: 1 }
+    validates :shipping_date_id,         numericality: { other_than: 1 }
+    validates :price,                    format: {  with: /\A[a-zA-Z0-9]+\z/
+                                                    greater_than_or_equal_to: 300
+                                                    less_than_or_equal_to: 9,999,999}
+  end
+
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable
 
+  has_many :products
+
   with_options presence: true do
     validates :name
     validates :email, uniqueness: { case_sensitive: false }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: 'は＠マークが必要です'}
