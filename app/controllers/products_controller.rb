db596eb6f085e4e0de01951d2ef0728930016503
nippi-29 <pipimelody.1726@gmail.com>
2020-09-14 15:11:27 +0900

    同じアクションをまとめる

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   def index
     @products = Product.all.order('created_at DESC')
@@ -17,19 +18,22 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @item = Product.find(params[:id])
   end
 
   def destroy
-    product = Product.find(params[:id])
-    if product.destroy
+    if @item.destroy
       redirect_to root_path
     else render :show
+    end
   end
 
   private
 
   def product_params
     params.require(:product).permit(:image, :name, :description, :category_id, :item_status_id, :postage_id, :region_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_product
+    @item = Product.find(params[:id])
   end
 end
