fc16ce948ae92afdd7e458f7d1f9948eb786bc02
nippi-29 <pipimelody.1726@gmail.com>
2020-09-14 20:57:39 +0900

    バリデーションの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,5 +3,12 @@ class Address < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to :transaction
 
-  
+  with_options presence: true do
+    validates :postcode,          format: { with:  /\A[0-9]{3}-[0-9]{4}\z/, message: "は-を含む半角数字7桁で入力してください"},
+    validates :prefecture_id,     numericality: { other_than: 1 },
+    validates :city
+    validates :block
+    validates :phone_number,      format: { with: /\A\d{10,11}\z/, message: 'は-を含まない10桁か11桁の半角英数字で入力してください'}
+  end
+
 end
